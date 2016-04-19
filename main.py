0d4fdfa7c673ef0c18707a5b4b57284f3ef26b6f
Michael Humphrey <mhumphree@gmail.com>
2016-04-19 13:28:29 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+Author: Michael Humphrey
+Description: This program demonstrates the following attributes...
+
+GUIs
+USB interfacing
+mySQL access
+Importing libraries locally
+Importing libraries via System includes
+Code repository use (Git)
+"""
+
+import Tkinter as Tk
